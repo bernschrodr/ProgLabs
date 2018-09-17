695daa5e78feb24b9a90f4a8ba551a3ad0e7e774
Vladislav <vlad60870@gmail.com>
2018-09-17 08:08:37 +0000

    lab3 var 15

---
+++ lab3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+//Вариант 15
+
+int main() {
+    int hex, oct;
+    printf("Введите целое шестнадцетиричное число");
+    scanf("%x", &hex);
+    printf("dec = %d \n", hex);
+    printf("oct = %o \n", hex);
+    printf("oct >> 3 = %o \n", hex >> 3);
+    printf("oct = %o \n", hex);
+    printf("!oct = %o \n", !hex);
+    scanf("%o", &oct);
+    printf("hex && oct = %o \n", hex && oct);
+
+    return 0;
+}
