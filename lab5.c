9600a47a2cff0dff1a3ab0b2f9ad0ba10e507918
Vladislav <vlad60870@gmail.com>
2018-10-01 03:17:18 -0700

    Update lab5.c

--- lab5.c
+++ lab5.c
@@ -4,11 +4,13 @@ int main() {
     int arr[9] = {99, 88, 77, 66, 55, 44, 33, 22, 11};
     int matrx[2][2] = {2, 5, 2, 2};
     int matrx2[2][2] = {1, 2, 0, 1};
-    int matrx_res[2][2];
+    int matrx_res[2][2] = {0,0,0,0};
 
     for (int i = 0; i < 9; i++) {
         printf("%d ", arr[i]);
     }
+    
+    printf("\n");
 
     for (int i = 0; i < 2; i++) {
         for (int j = 0; i < 2; i++){
@@ -16,5 +18,15 @@ int main() {
             matrx_res[i][j] += matrx[j][i] * matrx2[i][j];
         }
     }
+
+    for (int i = 0; i < 2; i++) {
+        printf("%d ", matrx_res[0][i]);
+    }
+    printf("\n");
+
+    for (int i = 0; i < 2; i++) {
+        printf("%d ", matrx_res[i][0]);
+    }
+
     return 0;
 }
