1c3b0c92fe985e0d7dc32050604ca2957ca71938
bernschrodr <vlad60870@gmail.com>
2018-10-29 10:32:11 +0300

    lab9

--- lab9.c
+++ lab9.c
@@ -4,5 +4,25 @@
 
 int main()
 {
-    
+    char Inp[256] = {0};
+    int i = 0,
+        dig = 0,
+        chr = 0,
+        Bchr = 0;
+    printf("Enter string\n");
+    scanf("%s", &Inp[0]);
+    while (Inp[i] != 0)
+    {
+        if (Inp[i] > 47 && Inp[i] < 58)
+            dig++;
+        else if (Inp[i] > 64 && Inp[i] < 91)
+            Bchr++;
+        else if (Inp[i] > 96 && Inp[i] < 123)
+            chr++;
+        i++;
+    }
+
+    printf("%d Digits\n", dig);
+    printf("%d Letter\n", chr);
+    printf("%d Big Letter", Bchr);
 }
