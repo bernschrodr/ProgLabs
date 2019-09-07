fab9543a16177c3adfb5180bc3f031faf92c5eee
Vladislav <vlad60870@gmail.com>
2019-09-07 13:13:03 +0300

    init 3sem lab1

---
+++ 3sem/lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace lab1
+{
+    class RationalFraction{
+        int m,n;
+        RationalFraction(int m, int n){
+            this.m = m;
+            this.n = n; 
+        }
+    }
+
+    class SetOfFractions{
+        HashSet<RationalFraction> set = new HashSet<RationalFraction>();
+        RationalFraction maxFraction = null;
+        RationalFraction minFraction = null; 
+
+        SetOfFractions(){
+        }
+
+        void add(RationalFraction fraction){
+            if(maxFraction != 0 )
+            set.Add(fraction);
+
+        }
+
+
+    }
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
